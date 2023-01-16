71506057be0c43e795e7546313fbce444345dc40
Serhiifcb <102388522+Serhiifcb@users.noreply.github.com>
2023-01-16 14:34:00 +0000

    Deploying to gh-pages from @ Serhiifcb/goit-react-hw-08-phonebook@9ebd3cceb97f066244b64fbbffdaf5c9d04a4bd4 🚀

---
+++ static/js/415.07bc36e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center",color:"rgb(116, 187, 201)"}};function i(){return(0,r.jsx)("div",{style:c.container,children:(0,r.jsxs)("h1",{style:c.title,children:["Phonebook welcome page"," "]})})}}}]);
+//# sourceMappingURL=415.07bc36e2.chunk.js.map
