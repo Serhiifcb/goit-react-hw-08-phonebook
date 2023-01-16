e1d608d729602607181f28d2a4545696f2aa520b
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2023-01-16 19:24:25 +0200

    added some style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { useAuth } from "hooks";
+import { NavLink } from "react-router-dom";
+
 const styles = {
   container: {
     minHeight: 'calc(100vh - 50px)',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -11,17 +15,22 @@ const styles = {
     textAlign: 'center',
     color: 'rgb(116, 187, 201)',
   },
+  notice: {
+    fontSize: 20,
+    fontWeight: 500,
+    textAlign: 'center',
+    color: 'rgb(116, 187, 201)',
+  }
 };
 
 export default function Home() {
+  const { isLoggedIn } = useAuth();
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        Phonebook welcome page{' '}
-        {/* <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span> */}
+        Phonebook
       </h1>
+      {!isLoggedIn ? (<p style={styles.notice}>Please <NavLink to="/register">register</NavLink> or <NavLink to="/login">login</NavLink> for using application</p>) : ('')}
     </div>
   );
 }
